39f46eb37395a724229861c1fa51a760463870d3
Holowjkeeee <2mad4m3@gmail.com>
2023-10-24 23:53:09 +0800

    Add particles infinite rendering

--- Form1.cs
+++ Form1.cs
@@ -7,24 +7,61 @@ namespace ParticleSystem
             InitializeComponent();
             // привязал изображение
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+            for (int i = 0; i < 50; i++)
+            {
+                particles.Add(new Particle());
+            }
         }
-        int counter = 0; // добавлю счетчик чтобы считать вызовы функции
+
+        private List<Particle> particles = new();
+
+        // добавил функцию обновления состояния системы
+        private void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                particle.Life -= 1; // уменьшаю здоровье
+                // если здоровье кончилось
+                if (particle.Life < 0)
+                {
+                    // восстанавливаю здоровье
+                    particle.Life = 20 + Particle.Rand.Next(100);
+                    // перемещаю частицу в центр
+                    particle.X = picDisplay.Image.Width / 2;
+                    particle.Y = picDisplay.Image.Height / 2;
+                    // делаю рандомное направление, скорость и размер
+                    particle.Direction = Particle.Rand.Next(360);
+                    particle.Speed = 1 + Particle.Rand.Next(10);
+                    particle.Radius = 2 + Particle.Rand.Next(10);
+                }
+                else
+                {
+                    // а это наш старый код
+                    var directionInRadians = particle.Direction / 180 * Math.PI;
+                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
+                    particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+                }
+            }
+        }
+
+        // функция рендеринга
+        private void Render(Graphics g)
+        {
+            // утащили сюда отрисовку частиц
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+        }
+
         private void timer1_Tick(object sender, EventArgs e)
         {
-            counter++; // увеличиваю значение счетчика каждый вызов
+            UpdateState();
+
             using var g = Graphics.FromImage(picDisplay.Image);
             // рисую на изображении сколько насчитал
             g.Clear(Color.White); // добавил очистку
-            g.DrawString(
-                counter.ToString(), // значения счетчика в виде строки
-                new Font("Arial", 12), // шрифт
-                new SolidBrush(Color.Black), // цвет
-                new PointF
-                { // по центру экрана
-                    X = picDisplay.Image.Width / 2,
-                    Y = picDisplay.Image.Height / 2
-                }
-            );
+            Render(g);
 
             // обновить picDisplay
             picDisplay.Invalidate();
--- Particle.cs
+++ Particle.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
 
 namespace ParticleSystem;
 
@@ -14,6 +15,7 @@ internal class Particle
 
     public float Direction; // направление движения
     public float Speed; // скорость перемещения
+    public float Life;
 
     // добавили генератор случайных чисел
     public static Random Rand = new();
@@ -25,6 +27,20 @@ internal class Particle
         Direction = Rand.Next(360);
         Speed = 1 + Rand.Next(10);
         Radius = 2 + Rand.Next(10);
+        Life = 20 + Rand.Next(100); // Добавили исходный запас здоровья от 20 до 120
+    }
+
+    public void Draw(Graphics g)
+    {
+        // создали кисть для рисования
+        var b = new SolidBrush(Color.Black);
+
+        // нарисовали залитый кружок радиусом Radius с центром в X, Y
+        g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+
+        // удалили кисть из памяти, вообще сборщик мусора рано или поздно это сам сделает
+        // но документация рекомендует делать это самому
+        b.Dispose();
     }
 }
 
