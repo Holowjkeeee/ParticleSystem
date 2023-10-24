5646ae0ea07ec7482a1dd39924e59de3bdd6a409
Holowjkeeee <2mad4m3@gmail.com>
2023-10-24 23:17:08 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace ParticleSystem
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
