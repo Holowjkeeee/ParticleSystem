f3f77adfa21c7b9b726d5fc8fa542bdd400902e8
Holowjkeeee <2mad4m3@gmail.com>
2023-10-25 00:16:15 +0800

    Add particle emitter

--- Form1.cs
+++ Form1.cs
@@ -7,88 +7,19 @@ public partial class Form1 : Form
         InitializeComponent();
         // прив€зал изображение
         picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
-        //for (int i = 0; i < 100; i++)
-        //{
-        //    particles.Add(new Particle(picDisplay.Image.Width / 2, picDisplay.Image.Height / 2));
-        //}
     }
 
-    private List<Particle> particles = new();
-    // добавл€ем переменные дл€ хранени€ положени€ мыши
-    private int MousePositionX = 0;
-    private int MousePositionY = 0;
+    ParticleEmitter emitter = new(); // добавили эмиттер
 
-
-    // добавил функцию обновлени€ состо€ни€ системы
-    private void UpdateState()
-    {
-        foreach (var particle in particles)
-        {
-            particle.Life -= 1; // уменьшаю здоровье
-            // если здоровье кончилось
-            if (particle.Life < 0)
-            {
-                // восстанавливаю здоровье
-                particle.Life = 20 + Particle.Rand.Next(100);
-                // перемещаю частицу в центр
-                //particle.X = picDisplay.Image.Width / 2;
-                //particle.Y = picDisplay.Image.Height / 2;
-                // новое начальное расположение частицы Ч это то, куда указывает курсор
-
-                particle.X = MousePositionX;
-                particle.Y = MousePositionY;
-                /* Ё“ќ ƒќЅј¬Ћяё, тут сброс состо€ни€ частицы */
-                var direction = (double)Particle.Rand.Next(360);
-                var speed = 1 + Particle.Rand.Next(10);
-
-                particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
-                particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
-
-                particle.Radius = 2 + Particle.Rand.Next(10);
-            }
-            else
-            {
-                // и добавл€ем новый, собственно он даже проще становитс€, 
-                // так как теперь мы храним вектор скорости в €вном виде и его не надо пересчитывать
-                particle.X += particle.SpeedX;
-                particle.Y += particle.SpeedY;
-            }
-        }
-
-        // добавил генерацию частиц
-        // генерирую не более 10 штук за тик
-        for (var i = 0; i < 10; ++i)
-        {
-            if (particles.Count > 500) return; // пока частиц меньше 500 генерируем новые
-            var particle = new ParticleColorful();
-            // ну и цвета мен€ем
-            particle.FromColor = Color.Yellow;
-            particle.ToColor = Color.FromArgb(0, Color.Magenta);
-            particle.X = MousePositionX;
-            particle.Y = MousePositionY;
-            particles.Add(particle);
-            
-        }
-    }
-
-    // функци€ рендеринга
-    private void Render(Graphics g)
-    {
-        // утащили сюда отрисовку частиц
-        foreach (var particle in particles)
-        {
-            particle.Draw(g);
-        }
-    }
 
     private void timer1_Tick(object sender, EventArgs e)
     {
-        UpdateState();
+        emitter.UpdateState(); // тут теперь обновл€ем эмиттер
 
         using var g = Graphics.FromImage(picDisplay.Image);
         // рисую на изображении сколько насчитал
         g.Clear(Color.Black); // добавил очистку
-        Render(g);
+        emitter.Render(g); // а тут теперь рендерим через эмиттер
 
         // обновить picDisplay
         picDisplay.Invalidate();
@@ -96,8 +27,8 @@ public partial class Form1 : Form
 
     private void picDisplay_MouseMove(object sender, MouseEventArgs e)
     {
-        // в обработчике заносим положение мыши в переменные дл€ хранени€ положени€ мыши
-        MousePositionX = e.X;
-        MousePositionY = e.Y;
+        // а тут в эмиттер передаем положение мыфки
+        emitter.MousePositionX = e.X;
+        emitter.MousePositionY = e.Y;
     }
 }
---
+++ ParticleEmitter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticleSystem;
+
+class ParticleEmitter
+{
+    List<Particle> particles = new();
+    public int MousePositionX;
+    public int MousePositionY;
+
+    public void UpdateState()
+    {
+        foreach (var particle in particles)
+        {
+            particle.Life -= 1; // уменьшаю здоровье
+            // если здоровье кончилось
+            if (particle.Life < 0)
+            {
+                // восстанавливаю здоровье
+                particle.Life = 20 + Particle.Rand.Next(100);
+                // перемещаю частицу в центр
+                //particle.X = picDisplay.Image.Width / 2;
+                //particle.Y = picDisplay.Image.Height / 2;
+                // новое начальное расположение частицы — это то, куда указывает курсор
+
+                particle.X = MousePositionX;
+                particle.Y = MousePositionY;
+                /* ЭТО ДОБАВЛЯЮ, тут сброс состояния частицы */
+                var direction = (double)Particle.Rand.Next(360);
+                var speed = 1 + Particle.Rand.Next(10);
+
+                particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+                particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+
+                particle.Radius = 2 + Particle.Rand.Next(10);
+            }
+            else
+            {
+                // и добавляем новый, собственно он даже проще становится, 
+                // так как теперь мы храним вектор скорости в явном виде и его не надо пересчитывать
+                particle.X += particle.SpeedX;
+                particle.Y += particle.SpeedY;
+            }
+        }
+
+        // добавил генерацию частиц
+        // генерирую не более 10 штук за тик
+        for (var i = 0; i < 10; ++i)
+        {
+            if (particles.Count > 500) return; // пока частиц меньше 500 генерируем новые
+            var particle = new ParticleColorful();
+            // ну и цвета меняем
+            particle.FromColor = Color.Yellow;
+            particle.ToColor = Color.FromArgb(0, Color.Magenta);
+            particle.X = MousePositionX;
+            particle.Y = MousePositionY;
+            particles.Add(particle);
+
+        }
+    }
+
+    public void Render(Graphics g)
+    {
+        // ну тут так и быть уж сам впишу...
+        // это то же самое что на форме в методе Render
+        foreach (var particle in particles)
+        {
+            particle.Draw(g);
+        }
+    }
+}
+
