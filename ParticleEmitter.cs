8e93bbf68e5012b0960934db20c1544869752e59
Holowjkeeee <2mad4m3@gmail.com>
2023-10-25 02:47:06 +0800

    Adjust base emitter

--- ParticleEmitter.cs
+++ ParticleEmitter.cs
@@ -2,6 +2,22 @@
 
 public class ParticleEmitter
 {
+    public int X; // координата X центра эмиттера, будем ее использовать вместо MousePositionX
+    public int Y; // соответствующая координата Y 
+    public int Direction = 0; // вектор направления в градусах куда сыпет эмиттер
+    public int Spreading = 360; // разброс частиц относительно Direction
+    public int SpeedMin = 1; // начальная минимальная скорость движения частицы
+    public int SpeedMax = 10; // начальная максимальная скорость движения частицы
+    public int RadiusMin = 2; // минимальный радиус частицы
+    public int RadiusMax = 10; // максимальный радиус частицы
+    public int LifeMin = 20; // минимальное время жизни частицы
+    public int LifeMax = 100; // максимальное время жизни частицы
+    public int ParticlesPerTick = 1; // добавил новое поле
+
+    public Color ColorFrom = Color.White; // начальный цвет частицы
+    public Color ColorTo = Color.FromArgb(0, Color.Black); // конечный цвет частиц
+
+
     List<Particle> particles = new();
     public int MousePositionX;
     public int ParticlesCount = 500;
@@ -12,13 +28,23 @@ public class ParticleEmitter
 
     public void UpdateState()
     {
+        int particlesToCreate = ParticlesPerTick; // фиксируем счетчик сколько частиц нам создавать за тик
+
         foreach (var particle in particles)
         {
             particle.Life -= 1; // уменьшаю здоровье
             // если здоровье кончилось
-            if (particle.Life < 0)
+            if (particle.Life <= 0) // если частицы умерла
             {
-                ResetParticle(particle); // заменили этот блок на вызов сброса частицы 
+                /* 
+                * то проверяем надо ли создать частицу
+                */
+                if (particlesToCreate > 0)
+                {
+                    /* у нас как сброс частицы равносилен созданию частицы */
+                    particlesToCreate -= 1; // поэтому уменьшаем счётчик созданных частиц на 1
+                    ResetParticle(particle);
+                }
             }
             else
             {
@@ -38,20 +64,15 @@ public class ParticleEmitter
             }
         }
 
-        // добавил генерацию частиц
-        // генерирую не более 10 штук за тик
-        for (var i = 0; i < 10; ++i)
+        // второй цикл меняем на while, 
+        // этот новый цикл также будет срабатывать только в самом начале работы эмиттера
+        // собственно пока не накопится критическая масса частиц
+        while (particlesToCreate >= 1)
         {
-            if (particles.Count > ParticlesCount) return; // пока частиц меньше 500 генерируем новые
-            var particle = new ParticleColorful();
-            // ну и цвета меняем
-            particle.FromColor = Color.Yellow;
-            particle.ToColor = Color.FromArgb(0, Color.Magenta);
-
-            ResetParticle(particle); // добавили вызов ResetParticle
-
+            particlesToCreate -= 1;
+            var particle = CreateParticle();
+            ResetParticle(particle);
             particles.Add(particle);
-
         }
     }
 
@@ -73,17 +94,36 @@ public class ParticleEmitter
     // добавил новый метод, виртуальным, чтобы переопределять можно было
     public virtual void ResetParticle(Particle particle)
     {
-        particle.Life = 20 + Particle.Rand.Next(100);
-        particle.X = MousePositionX;
-        particle.Y = MousePositionY;
+        //particle.Life = 20 + Particle.Rand.Next(100);
+        particle.Life = Particle.Rand.Next(LifeMin, LifeMax);
+        //particle.X = MousePositionX;
+        //particle.Y = MousePositionY;
+        particle.X = X;
+        particle.Y = Y;
 
-        var direction = (double)Particle.Rand.Next(360);
-        var speed = 1 + Particle.Rand.Next(10);
+        var direction = Direction
+                        + (double)Particle.rand.Next(Spreading)
+                        - Spreading / 2;
+
+        var speed = Particle.rand.Next(SpeedMin, SpeedMax);
 
         particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
         particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
 
-        particle.Radius = 2 + Particle.Rand.Next(10);
+        particle.Radius = Particle.Rand.Next(RadiusMin, RadiusMax);
     }
+
+    /* добавил метод */
+    public virtual Particle CreateParticle()
+    {
+        var particle = new ParticleColorful
+        {
+            FromColor = ColorFrom,
+            ToColor = ColorTo
+        };
+
+        return particle;
+    }
+
 }
 
